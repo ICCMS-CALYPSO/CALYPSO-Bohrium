b1e2a8af3f608815170f2d71a030fd795a8eccce
wangzy <zhenyu wang>
2023-10-18 10:38:17 +0800

    fix: consider the  in parameters

--- calypso_bohrium/utils.py
+++ calypso_bohrium/utils.py
@@ -4,6 +4,7 @@ import os
 
 
 def get_value(key):
-    temp = os.popen(f"grep {key} input.dat").read().split("=")[-1].strip()
+    # temp = os.popen(f"grep {key} input.dat -i ").read().split("=")[-1].strip()
+    temp = os.popen(f"grep {key} input.dat -i ").read().split("#")[0].split("=")[-1].strip()
     return temp
 
